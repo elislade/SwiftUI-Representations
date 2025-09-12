7d80d3cb3571eaf5c9658e980323f0f7e3b8cc35
Eli Slade <elislade@gmail.com>
2025-09-12 16:04:29 -0700

    Updated Slider.

--- Sources/ExampleRepresentations/Shared/Slider.swift
+++ Sources/ExampleRepresentations/Shared/Slider.swift
@@ -4,7 +4,7 @@ struct Slider<Value: BinaryFloatingPoint>: View where Value.Stride: BinaryFloati
     
     let value: Binding<Value>
     var `in`: ClosedRange<Value>? = nil
-    var step: Value? = nil
+    var step: Value.Stride? = nil
     
     var body: some View {
         #if os(tvOS)
@@ -15,7 +15,15 @@ struct Slider<Value: BinaryFloatingPoint>: View where Value.Stride: BinaryFloati
             EmptyView()
         }
         #else
-        SwiftUI.Slider(value: value)
+        if let range = `in` {
+            if let step {
+                SwiftUI.Slider(value: value, in: range, step: step)
+            } else {
+                SwiftUI.Slider(value: value, in: range)
+            }
+        } else {
+            SwiftUI.Slider(value: value)
+        }
         #endif
     }
     
