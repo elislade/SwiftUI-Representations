a4beb4a38f3ecfa4b3a70aa343e01097e3264df5
Eli Slade <elislade@gmail.com>
2025-09-12 16:06:17 -0700

    Updated: TabViewRepresentation now uses custom controller instead UITabBarController.

--- Sources/UIKitRepresentations/Controllers/TabViewRepresentation.swift
+++ Sources/UIKitRepresentations/Controllers/TabViewRepresentation.swift
@@ -65,44 +65,167 @@ public final class CustomTabBarController: UIViewController {
     
     public override func viewDidLoad() {
         guard children.indices.contains(selectedIndex) else { return }
+        
         view.addSubview(children[selectedIndex].view)
         children[selectedIndex].didMove(toParent: self)
     }
     
 }
 
+extension TabViewRepresentation: UIViewControllerRepresentable {
+    
+    public func makeUIViewController(context: Context) -> CustomTabBarController {
+        let ctrl = CustomTabBarController()
+        for tab in tabs {
+            let c = TabWrapperViewController(tab)
+            ctrl.addChild(c)
+            c.didMove(toParent: ctrl)
+        }
+        return ctrl
+    }
+    
+    public func updateUIViewController(_ uiViewController:CustomTabBarController, context: Context) {
+        if index != uiViewController.selectedIndex {
+            uiViewController.selectedIndex = index
+        }
+    }
+    
+}
+
+
+final class TabWrapperViewController: UIViewController {
+    
+    
+    let backing: UIViewController
+    
+    private var hasBecomeVisible = false
+    
+    init(_ backing: UIViewController) {
+        self.backing = backing
+        super.init(nibName: nil, bundle: nil)
+        addChild(backing)
+        backing.didMove(toParent: self)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func loadView() {
+        view = View(backing.view)
+    }
+    
+    override func viewIsAppearing(_ animated: Bool) {
+        guard !hasBecomeVisible else { return }
+        super.viewIsAppearing(animated)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        view.isHidden = false
+        guard !hasBecomeVisible else { return }
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        guard !hasBecomeVisible else { return }
+        super.viewDidAppear(animated)
+        hasBecomeVisible = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        guard !hasBecomeVisible else { return }
+        super.viewWillDisappear(animated)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        guard !hasBecomeVisible else { return }
+        super.viewDidDisappear(animated)
+    }
+    
+    
+    final class View: UIView {
+        
+        let backing: UIView
+        
+        override var isHidden: Bool {
+            didSet {
+                backing.isHidden = isHidden
+                backing.frame = bounds
+            }
+        }
+        
+        init(_ backing: UIView) {
+            self.backing = backing
+            super.init(frame: .zero)
+            addSubview(backing)
+        }
+        
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+        
+        override func removeFromSuperview() {
+            // don't remove from view so onDisappear and onAppear don't get called for every tab switch.
+            isHidden = true
+        }
+        
+        override func layoutSubviews() {
+            if isHidden { return }
+            super.layoutSubviews()
+            CATransaction.begin()
+            CATransaction.setAnimationDuration(0)
+            backing.frame = bounds
+            CATransaction.commit()
+        }
+        
+        override func layoutIfNeeded() {
+            if isHidden { return }
+            super.layoutIfNeeded()
+        }
+        
+        override func setNeedsLayout() {
+            if isHidden { return }
+            super.setNeedsLayout()
+        }
+        
+        override func setNeedsDisplay() {
+            if isHidden { return }
+            super.setNeedsDisplay()
+        }
+        
+        override func setNeedsUpdateConstraints() {
+            if isHidden { return }
+            super.setNeedsUpdateConstraints()
+        }
+        
+        override func safeAreaInsetsDidChange() {
+            if isHidden { return }
+            super.safeAreaInsetsDidChange()
+        }
+        
+    }
+
+    
+}
+
 //extension TabViewRepresentation: UIViewControllerRepresentable {
 //    
-//    public func makeUIViewController(context: Context) -> CustomTabBarController {
-//        let ctrl = CustomTabBarController()
-//        for tab in tabs { ctrl.addChild(tab) }
+//    public func makeUIViewController(context: Context) -> UITabBarController {
+//        let ctrl = UITabBarController()
+//        ctrl.view.backgroundColor = .clear
+//        ctrl.setViewControllers(tabs, animated: false)
+//        if #available(iOS 18.0, tvOS 18.0, visionOS 2.0, *) {
+//            ctrl.isTabBarHidden = true
+//        }
+//        ctrl.tabBar.isHidden = true
 //        return ctrl
 //    }
 //    
-//    public func updateUIViewController(_ uiViewController:CustomTabBarController, context: Context) {
+//    public func updateUIViewController(_ uiViewController: UITabBarController, context: Context) {
 //        uiViewController.selectedIndex = index
 //    }
 //    
 //}
-
-extension TabViewRepresentation: UIViewControllerRepresentable {
-    
-    public func makeUIViewController(context: Context) -> UITabBarController {
-        let ctrl = UITabBarController()
-        ctrl.view.backgroundColor = .clear
-        ctrl.setViewControllers(tabs, animated: false)
-        if #available(iOS 18.0, tvOS 18.0, visionOS 2.0, *) {
-            ctrl.isTabBarHidden = true
-        }
-        ctrl.tabBar.isHidden = true
-        return ctrl
-    }
-    
-    public func updateUIViewController(_ uiViewController: UITabBarController, context: Context) {
-        uiViewController.selectedIndex = index
-    }
-    
-}
 
 #elseif canImport(AppKit)
 
